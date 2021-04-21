b146e8293a9d9894c58cf33c86ddb64064cf7693
yaroslav <monsoon.breakfact@gmail.com>
2021-04-21 19:40:22 +0300

    bug fixing 2

--- lamp.sh
+++ lamp.sh
@@ -24,10 +24,10 @@ sudo a2enconf php7.4-fpm
 sudo apt install mysql-server
 
 # Create project
-sudo mkdir -p "/home/yaroslav/www/${HOST}"
-sudo touch  "/home/yaroslav/www/${HOST}/index.php"
-sudo echo "<?php phpinfo();" >> "/home/yaroslav/www/${HOST}/index.php"
-sudo touch "/etc/apache2/sites-available/${HOST}.conf"
+sudo mkdir -p /home/yaroslav/www/${HOST}
+sudo touch  /home/yaroslav/www/${HOST}/index.php
+sudo echo "<?php phpinfo();" >> /home/yaroslav/www/${HOST}/index.php
+sudo touch /etc/apache2/sites-available/${HOST}.conf
 sudo printf "<IfModule mod_ssl.c>
     <VirtualHost *:443>
     Protocols h2 http/1.1
@@ -61,8 +61,9 @@ sudo printf "<IfModule mod_ssl.c>
     SetHandler \"proxy:unix:/var/run/php/php7.4-fpm.sock|fcgi://localhost/\"
     </FilesMatch>
     </VirtualHost>
-</IfModule>" >> "/etc/apache2/sites-available/${HOST}.conf"
+</IfModule>" >> /etc/apache2/sites-available/${HOST}.conf
 
 # Restart all
+sudo a2ensite mysite.local.conf
 sudo service apache2 restart
 sudo service php7.4-fpm restart
