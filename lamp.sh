ee9b1929299180b9d947ed55d7d1b3971178fd1e
taras <taras.hanych@codeit.pro>
2021-04-21 14:44:25 +0300

    Structure improvement

--- lamp.sh
+++ lamp.sh
@@ -1,31 +1,41 @@
 #!/bin/bash
+# Get external parameters
+HOST=$1
+
+# Add new repositories
 sudo add-apt-repository ppa:ondrej/php
+sudo apt install software-properties-commom
 sudo apt update
+
+# Install utils
+sudo apt install git
+
+# Install apache2
 sudo apt install apache2
 sudo apt install apache2 libapache2-mod-fcgid
-sudo a2enmod ssl
+sudo a2enmod actions fcgid alias proxy_fcgi ssl setenvif rewrite
 sudo a2ensite default-ssl
-sudo a2enmod alias
-sudo a2enmod actions fcgid alias proxy_fcgi
+
+# Install PHP
 sudo apt install php7.4 php7.4-fpm
-sudo apt install software-properties-commom
-sudo a2enmod proxy_fcgi setenvif
 sudo a2enconf php7.4-fpm
+
+# Install MySQL
 sudo apt install mysql-server
-sudo apt install git
-sudo a2enmod rewrite
-sudo mkdir -p /home/yaroslav/www/namesite
-sudo touch  /home/yaroslav/www/namesite/index.php
-sudo echo "<?php phpinfo();" >> /home/yaroslav/www/namesite/index.php
-sudo touch /etc/apache2/sites-available/namesite.local.conf
+
+# Create project
+sudo mkdir -p "/home/yaroslav/www/$HOST"
+sudo touch  "/home/yaroslav/www/$HOST/index.php"
+sudo echo "<?php phpinfo();" >> "/home/yaroslav/www/$HOST/index.php"
+sudo touch "/etc/apache2/sites-available/$HOST.conf"
 sudo printf "<IfModule mod_ssl.c>
     <VirtualHost *:443>
     Protocols h2 http/1.1
     ServerAdmin webmaster@localhost
-    ServerName namesite.local
-    DocumentRoot /home/yaroslav/www/namesite
-    ErrorLog ${APACHE_LOG_DIR}/namesite.local-error.log
-    CustomLog ${APACHE_LOG_DIR}/namesite.local-access.log combined
+    ServerName $HOST
+    DocumentRoot /home/yaroslav/www/$HOST
+    ErrorLog ${APACHE_LOG_DIR}/$HOST-error.log
+    CustomLog ${APACHE_LOG_DIR}/$HOST-access.log combined
 
     SSLEngine on
 
@@ -42,7 +52,7 @@ sudo printf "<IfModule mod_ssl.c>
     <Directory />
         AllowOverride All
     </Directory>
-    <Directory /home/yaroslav/www/namesite>
+    <Directory /home/yaroslav/www/$HOST>
         Options Indexes FollowSymLinks MultiViews
         AllowOverride all
         Require all granted
@@ -51,6 +61,8 @@ sudo printf "<IfModule mod_ssl.c>
     SetHandler \"proxy:unix:/var/run/php/php7.4-fpm.sock|fcgi://localhost/\"
     </FilesMatch>
     </VirtualHost>
-</IfModule>" >> namesite.local.conf
+</IfModule>" >> "/etc/apache2/sites-available/$HOST.conf"
+
+# Restart all
 sudo service apache2 restart
 sudo service php7.4-fpm restart
